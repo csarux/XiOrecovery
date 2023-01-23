a579acedfe880190c2fe08a195d8e0b65ae8c16a
csarux <cesaro02@ucm.es>
2023-01-23 09:48:20 +0100

    Correct dcmcoher.py

--- scripts/dcmcoher.py
+++ scripts/dcmcoher.py
@@ -49,7 +49,7 @@ def correctImagePositionPatientInCTImages(dcmdir='xiodcm', prefct = 'image', del
     """
     
     with open(deltaframes, mode = 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+        csv_reader = csv.DictReader(csv_file)
    
     for row in csv_reader:
         filePatientID, CT, X, Y = row['PatientID'], row['CT'], row['X'], row['Y']
